a74df3982e3be6353d251b004c85b28c42d3713a
xingchen <664949079@qq.com>
2018-12-22 19:42:29 +0800

    给实体类创建映射关系，填写配置文件

--- jpa_day01_quickstart/src/main/java/com/itheima/domain/Customer.java
+++ jpa_day01_quickstart/src/main/java/com/itheima/domain/Customer.java
@@ -1,5 +1,6 @@
 package com.itheima.domain;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -7,15 +8,34 @@ import java.io.Serializable;
  * @version: 1.0
  * @date: 2018/12/22
  * @since: 1.0
+ *
+ * 所有的注解都是使用JPA的规范提供的注解
+ * 所以在导入注解包的时候，一定要统一导入javax.persistence下的
  */
-
+@Entity//声明实体类
+@Table(name = "cst_customer")//建立实体类和表的映射关系
 public class Customer implements Serializable {
+    @Id//声明当前私有属性为主键
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//配置主键的生成策略，IDENTITY表示自增长
+    @Column(name = "cust_id")//指定和表中cust_id字段的映射关系
     private Long custId;
+
+    @Column(name = "cust_name")//指定和表中cust_name字段的映射关系
     private String custName;
+
+    @Column(name = "cust_source")//指定和表中cust_source字段的映射关系
     private String custSource;
+
+    @Column(name = "cust_industry")//指定和表中的cust_industry字段的映射关系
     private String custIndustry;
+
+    @Column(name = "cust_level")//
     private String custLevel;
+
+    @Column(name = "cust_address")//指定和表中cust_address字段的映射关系
     private String custAddress;
+
+    @Column(name="cust_phone")//指定和表中cust_phone字段的映射关系
     private String custPhone;
 
     public Long getCustId() {
